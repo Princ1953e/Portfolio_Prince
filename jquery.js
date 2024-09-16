b21a307be4dce856c31bc3088a547eaa18e82e2d
Princ1953e <princ1953e@gmail.com>
2024-09-16 12:25:21 +0530

    Portfolio

---
+++ jquery.js
@@ -0,0 +1,14 @@
+$(window).on('load', function(){
+    $(".web-loader").delay(3000).fadeOut("slow")
+})
+
+
+var cursor = document.querySelector('.cursor');
+var a = document.querySelectorAll('a');
+
+document.addEventListener('mousemove', function(e){
+  var x = e.clientX;
+  var y = e.clientY;
+  cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`
+});
+
